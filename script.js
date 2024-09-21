c8e59a9f6ab7697d1b1abf4bf9680b0a95931b21
Pradumn Dubey <140749011+dubeypradum999@users.noreply.github.com>
2024-09-22 02:13:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function rotateLine() {
+  var line = document.getElementById("line");
+  var angle = 0;
+
+  setInterval(function() {
+    angle += 2;
+    line.style.transform = "rotate(" + angle + "deg)";
+  }, 20);
+}
+
+rotateLine();
